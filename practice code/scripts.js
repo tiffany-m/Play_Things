9fdc07bc5aeab83fc14334465824cb21f340034f
Tiffany <tiffmollica@gmail.com>
2021-07-23 18:58:29 -0400

    try these

--- practice code/scripts.js
+++ practice code/scripts.js
@@ -248,3 +248,23 @@ function changeText() {
 
 changeBtn.addEventListener('click', changeText)
 
+
+
+let fizzBuzz = () => {
+  for(let i = 1; i <= 100; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz')
+    } else if (i % 3 === 0) {
+      console.log('Fizz')
+    } else if (i % 5 === 0) {
+      console.log('Buzz')
+    } else {
+      console.log(i)
+    }
+  }
+}
+
+fizzBuzz()
+
+https://medium.com/@sojibrahmatuzzaman/10-common-javascript-interview-challenges-f2547db5370b
+
