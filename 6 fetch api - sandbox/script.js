fb936104cb671933b6b5f55579844a7878e90676
Tiffany <tiffmollica@gmail.com>
2021-08-08 14:57:10 -0400

    added get api data button

--- 6 fetch api - sandbox/script.js
+++ 6 fetch api - sandbox/script.js
@@ -1,5 +1,6 @@
 document.getElementById('getText').addEventListener('click', getText)
 document.getElementById('getUsers').addEventListener('click', getUsers)
+document.getElementById('getPosts').addEventListener('click', getPosts);
 
 // function getText() {
 //   fetch('sample.txt')
@@ -38,4 +39,21 @@ function getUsers() {
     })
     document.getElementById('output').innerHTML = output
   })
+}
+
+function getPosts() {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((res) => res.json())
+  .then((data) => {
+    let output = '<h2>Posts</h2>';
+    data.forEach(function(post) {
+      output += `
+        <div>
+          <h3>${post.title}</h3>
+          <p>${post.body}</p>
+        </div>
+    `;
+    });
+    document.getElementById('output').innerHTML = output;
+  });
 }
